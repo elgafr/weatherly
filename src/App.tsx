eb3d91b049f47f9c79e176043863de11bcfefa41
elgafr <elgaputrifr@gmail.com>
2024-12-03 22:39:11 +0700

    update queries for api

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,35 @@ import { ThemeProvider } from "./context/theme-provider";
 import Layout from "./components/layout";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city-page";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      cacheTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-
-    
-    <BrowserRouter>
-      <ThemeProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-</QueryClientProvider>
-
+      <BrowserRouter>
+        <ThemeProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
