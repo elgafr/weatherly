86fc275de620bcd4fdda4cb85366d5d1e5517219
elgafr <elgaputrifr@gmail.com>
2024-12-09 23:13:39 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(dirname(fileURLToPath(import.meta.url)), './src'), 
     },
   },
 })
